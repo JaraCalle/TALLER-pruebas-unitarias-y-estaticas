2c4a8e2adc8937ec5ad9fd9ab8260c7425e543f5
Juan Jara <jaracalle2041@gmail.com>
2026-03-04 10:18:41 -0500

    feat: Punto 6 del taller completo

--- js_app/tests/stringUtils.test.js
+++ js_app/tests/stringUtils.test.js
@@ -1,7 +1,29 @@
-
 // TODO: Escribe pruebas unitarias para isPalindrome.
 // Sugerencias: "radar" -> true; "anita lava la tina" -> true; "python" -> false; "" -> true; "Radar" -> true
+
+const { isPalindrome } = require('../src/stringUtils')
 
 test('ejemplo siembra', () => {
   expect(true).toBe(true);
 });
+
+test('radar', () => {
+  expect(true).toBe(isPalindrome('radar'))
+});
+
+test('anita lava la tina', () => {
+  expect(true).toBe(isPalindrome('anita lava la tina'))
+});
+
+test('python', () => {
+  expect(false).toBe(isPalindrome('python'))
+});
+
+test('', () => {
+  expect(true).toBe(isPalindrome(''))
+});
+
+test('Radar', () => {
+  expect(true).toBe(isPalindrome('Radar'))
+});
+
