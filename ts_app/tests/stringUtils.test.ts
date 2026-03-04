62ccbe732d2ef0571a48ae3a2b59d21e810b6360
Juan Jara <jaracalle2041@gmail.com>
2026-03-04 10:28:12 -0500

    Feat: punto 7 Completado

--- ts_app/tests/stringUtils.test.ts
+++ ts_app/tests/stringUtils.test.ts
@@ -1,11 +1,41 @@
-
 import { describe, it, expect } from 'vitest'
 import { isPalindrome } from '../src/stringUtils'
 
-// TODO: completa los casos: "radar"->true, "anita lava la tina"->true, "python"->false, ""->true, "Radar"->true
+// Sugerencias: "Radar" -> true; "anita lava la tina" -> true; "python" -> false; "" -> true; "Radar" -> true
+
 
 describe('siembra', () => {
   it('ejemplo', () => {
     expect(true).toBe(true)
   })
 })
+
+describe('radar', () => {
+  it('radar', () => {
+    expect(true).toBe(isPalindrome('radar'))
+  })
+});
+
+describe('anita lava la tina', () => {
+  it('anita lava la tina', () => {
+    expect(true).toBe(isPalindrome('anita lava la tina'))
+  })
+});
+
+describe('python', () => {
+  it('python', () => {
+    expect(false).toBe(isPalindrome('python'))
+  })
+});
+
+describe('', () => {
+  it('', () => {
+    expect(true).toBe(isPalindrome(''))
+  })
+});
+
+describe('Radar', () => {
+  it('Radar', () => {
+    expect(true).toBe(isPalindrome('Radar'))
+  })
+});
